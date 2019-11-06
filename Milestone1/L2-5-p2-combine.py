b34d7be788b0e0cdf6dc52965c8797d91ceb51ce
Anthony Luo <anthonyluo100@gmail.com>
2019-11-06 08:42:34 -0500

    Added combine

---
+++ Milestone1/L2-5-p2-combine.py
@@ -0,0 +1,5 @@
+from Cimpl import load_image, create_color, set_color, show, Image, save_as
+
+r_img = load_image('red_channel.jpg')
+g_img = load_image('green_channel.jpg')
+b_img = load_image('blue_channel.jpg')

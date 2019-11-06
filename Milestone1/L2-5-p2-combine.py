7253c076ae93e9a8ad541d68c09247cd850c04b1
Anthony Luo <anthonyluo100@gmail.com>
2019-11-06 09:50:58 -0500

    Combine works

--- Milestone1/L2-5-p2-combine.py
+++ Milestone1/L2-5-p2-combine.py
@@ -4,6 +4,8 @@ r_img = load_image('red_channel.jpg')
 g_img = load_image('green_channel.jpg')
 b_img = load_image('blue_channel.jpg')
 
+new_img = r_img
+
 r_chan = []
 g_chan = []
 b_chan = []
@@ -13,6 +15,16 @@ for x, y, (r, g, b) in r_img:
 
 for x, y, (r, g, b) in g_img:
     g_chan.append(g)
-    
+
 for x, y, (r, g, b) in b_img:
     b_chan.append(b)
+
+counter = 0
+for x, y, (r, g, b) in new_img:
+    colour = create_color(r_chan[counter], g_chan[counter], b_chan[counter])
+    set_color(new_img, x, y, colour)
+    counter += 1
+
+save_as(new_img, 'new_image.jpg')
+show(load_image('new_image.jpg'))
+

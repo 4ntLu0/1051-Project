1bdc2467811c9a0cf37502e31771f3919d754400
Anthony Luo <anthonyluo100@gmail.com>
2019-11-07 16:33:36 -0500

    yote

--- Milestone1/L2_5_P2_channel_filters.py
+++ Milestone1/L2_5_P2_channel_filters.py
@@ -1,9 +1,8 @@
 from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height
 L2_5_P2_red = __import__('L2-5-P2-red')
 L2_5_P2_green = __import__('L2-5-P2-green')
-L2_5_P2_blue = __import__('L2-5-P2-blue')
-L2_5_P2_combine = __import__('L2-5-P2-combine')
-
+L2_5_P2_blue = __import__('blueTEST')
+#
 
 original_img = 'p2-original.jpg'
 
@@ -11,11 +10,8 @@ if __name__ == '__main__':
     """main function to run all the things!
     Written by Anthony Luo
     """
-    L2_5_P2_red.createRed(original_img)
-    L2_5_P2_green.createGreen(original_img)
+    print('running red')
+    L2_5_P2_red.createRed(original_img, False)
+    print('done red')
+    #L2_5_P2_green.createGreen(original_img)
     L2_5_P2_blue.createBlue(original_img)
-    L2_5_P2_combine.combine()
-    if input('Run testing? Y / N: ') == Y:
-        testing()
-    else:
-        pass

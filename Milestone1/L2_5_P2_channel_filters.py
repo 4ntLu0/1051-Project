5fc4f11f65609b997c915f32a0e5fec4020a1dfe
Anthony Luo <anthonyluo100@gmail.com>
2019-11-07 17:25:58 -0500

    Added red testing

--- Milestone1/L2_5_P2_channel_filters.py
+++ Milestone1/L2_5_P2_channel_filters.py
@@ -1,7 +1,7 @@
 from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height
 from L2_5_P2_red import createRed
 from L2_5_P2_green import green_filter
-from L2_5_P2_blue import createBlue
+#from L2_5_P2_blue import createBlue
 
 original_img = 'p2-original.jpg'
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     Written by Anthony Luo
     """
     print('running red')
-    createRed(original_img, False)
+    createRed(original_img, True)
     print('done red')
     green_filter(original_img)
-    createBlue(original_img)
+    #createBlue(original_img)

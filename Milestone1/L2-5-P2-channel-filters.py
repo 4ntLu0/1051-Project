3abd2c6c232b58568e86eb55e55d048cef8e5061
Anthony Luo <anthonyluo100@gmail.com>
2019-11-07 12:43:20 -0500

    FIXED IMPORTING

--- Milestone1/L2-5-P2-channel-filters.py
+++ Milestone1/L2-5-P2-channel-filters.py
@@ -1,6 +1,7 @@
 from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height
-from L2_5_P2_red import createRed
-original_img = load_image('p-2-original.jpg')
+L2_5_P2_red = __import__('L2-5-P2-red')
+#from L2_5_P2_red import createRed
+original_img = 'p2-original.jpg'
 
 if __name__ == '__main__':
-    createRed(original_img, True, False)
+    L2_5_P2_red.createRed(original_img, True, False)

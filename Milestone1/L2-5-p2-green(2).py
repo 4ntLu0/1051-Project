405cd1060b1000eeda13c7b71318dbce22ae0c0c
Alia79 <57446174+Alia79@users.noreply.github.com>
2019-11-07 17:30:32 -0500

    Update L2-5-p2-green(2).py
    works

--- Milestone1/L2-5-p2-green(2).py
+++ Milestone1/L2-5-p2-green(2).py
@@ -1,12 +1,13 @@
 from Cimpl import *
 
-image = load_image('p2-original.jpg') #loads the desired image
+
 
 def green_filter(image: Image) -> Image:
     """Alia Nichol
     
     """
-    
+    image = load_image('p2-original.jpg') #loads the desired image
+    show(image) 
     image1 = copy(image) #creates a copy of the image so it is not overrided
     
     
@@ -14,21 +15,18 @@ def green_filter(image: Image) -> Image:
         green = create_color(0, g, 0)
         set_color(image1, x, y, green) # sets all the pixels of defined locations in that image to the color
         
-    show(image1)
-    return image1
-
-save_as(image, 'green_channel.jpg')  # saves as a new image
-if verify:
+  
+    save_as(image1, 'green_channel.jpg')  # saves as a new image
     show(load_image('green_channel.jpg'))  # shows the image to double check
 
-print('green_channel created')
+    print('green_channel created')
 
-
+    return image1
 
 def test_green() -> None:
     '''Test function for green filter that tests if all the pixels in the image are green. The function returns 'PASS' if all pixels successfully change to green. If not, the function returns 'FAIL'.
     '''
-    image1 = green_filter(image)
+    image1 = green_filter('p2-original.jpg')
    
     for x,y,(r, g, b) in image1:
         if r ==0 and b ==0:

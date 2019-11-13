03c3c0f96ba588d45e0952c38ac620c57cde985a
Anthony Luo <anthonyluo100@gmail.com>
2019-11-13 09:42:02 -0500

    Sepia!!!!

--- Milestone2/L2_5_sepia.py
+++ Milestone2/L2_5_sepia.py
@@ -1,2 +1,20 @@
-from Utils.Cimpl import * #change this later please
+from Utils.Cimpl import load_image, choose_file, show, set_color, save_as, create_color #change this later please
+from Utils.simple_Cimpl_filters import grayscale
+from typing import NewType
+Image = NewType('Image', str)
 
+
+def sepia(sep_img):
+    for x, y, (r, g, b) in sep_img:
+        if r < 63:
+            set_color(sep_img, x, y, create_color(r * 1.1, g, b * 0.9))
+        elif r <= 191:
+            set_color(sep_img, x, y, create_color(r * 1.15, g, b * 0.85))
+        else:
+            set_color(sep_img, x, y, create_color(r * 1.08, g, b * 0.93))
+    show(sep_img)
+    return sep_img
+
+if __name__ == '__main__':
+    working_img = grayscale(load_image(choose_file()))
+    save_as(sepia(working_img), 'returns/sepia.jpg')

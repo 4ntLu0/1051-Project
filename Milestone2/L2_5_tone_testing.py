b0ec73d60d39010881faa52b7bc6aff0c2f736cc
Anthony Luo <anthonyluo100@gmail.com>
2019-11-14 12:30:43 -0500

    Moved some folders

---
+++ Milestone2/L2_5_tone_testing.py
@@ -0,0 +1,8 @@
+from Utils.Cimpl import load_image, choose_file, show, set_color, save_as, create_color #change this later please
+from Utils.simple_Cimpl_filters import grayscale
+from typing import NewType
+Image = NewType('Image', str)
+from datetime import datetime
+
+# TODO: write the rest of this code :(
+

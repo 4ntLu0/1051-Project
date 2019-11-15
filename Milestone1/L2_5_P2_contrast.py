5946594a8eff98755158b4321b27118afed1baaf
Alia79 <57446174+Alia79@users.noreply.github.com>
2019-11-15 13:14:19 -0500

    HIIII I UPDATED THE CONTRAST. MY OTHER ONE WASN'T RIGHT

--- Milestone1/L2_5_P2_contrast.py
+++ Milestone1/L2_5_P2_contrast.py
@@ -2,14 +2,46 @@ from Cimpl import *
 
 image = load_image(choose_file())  # loads the desired image from a given file
 show(image)
-image1 = copy(image) #creates a copy of the image so it is not overrided when the new image is returned
 
-def extreme_contrast(image):
+image1 = copy(image) #creates a copy of the image so it is not overrided when the new image is returned"""
+
+
+
+def extreme_contrast(image: Image) -> Image:
+    
     """ Given an 
     Written by Alia Nichol
-    """    
+    """   
+    for pixel in image1:
+        x, y, (r, g, b) = pixel
+        
+        if 0 < r < 127:
+            r = 0 
+        else: 
+            r = 255
+        
+        if 0 < g < 127:
+            g = 0
+        else:
+            g = 255
+        
+        if 0 < b < 127:
+            b = 0
+        else:
+            b = 255
+    
+    
+        set_color(image1, x, y, create_color(r,g,b))
+    
+    show(image1)  
+    return image1
+    
 
-    for x, y, (r, g, b) in image:  # reads through each pixel in the image
+""" DOES NOT WORK BUT NOT SURE WHY
+
+for x, y, (r, g, b) in image:  # reads through each pixel in the image
+       
+       
         if 0 < r < 127:
             contrast = create_color(0, g, b)
             set_color(image, x, y, contrast)            
@@ -33,5 +65,5 @@ def extreme_contrast(image):
     show(load_image('contrast_channel.png'))
     print('contrast_channel saved as new image')
     
-    return image1
+    return image1"""
     

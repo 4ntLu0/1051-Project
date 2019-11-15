580211cb09d7278673edb74156fe9ae040c041df
Anthony Luo <anthonyluo100@gmail.com>
2019-11-15 13:40:17 -0500

    aeou

--- Milestone2/L2_5_tone_testing.py
+++ Milestone2/L2_5_tone_testing.py
@@ -1,7 +1,8 @@
-from Utils.Cimpl import load_image, choose_file, show, set_color, save_as, create_color #change this later please
+from Utils.Cimpl import load_image, choose_file, show, set_color, save_as, create_color, create_image #change this
+# later please
 from Utils.simple_Cimpl_filters import grayscale
 from typing import NewType
-from L2_5_two_tone import twoTone
+#from L2_5_two_tone import twoTone
 from L2_5_three_tone import threeTone
 Image = NewType('Image', str)
 from datetime import datetime
@@ -10,7 +11,7 @@ from datetime import datetime
 
 tones = {'black' : (0, 0, 0), 'white': (255, 255, 255), 'red': (255, 0, 0), 'lime': (0, 255, 0), 'blue': (0, 0, 255),
          'yellow': (255, 255, 0), 'cyan': (0, 255, 255), 'magenta': (255, 0, 255), 'gray': (128, 128, 128)}
-
+toneList = ['black', 'white', 'red', 'lime', 'blue', 'yellow', 'cyan', 'magenta', 'gray']
 
 def testTwoTone():
     ''' Tests to ensure that two tone functions
@@ -26,3 +27,10 @@ def testThreeTone():
     :return:
     :rtype:
     '''
+    ttti = create_image(3,1) #creates image
+    set_color(ttti, 0, 0, create_color(0, 0, 0))
+    set_color(ttti, 1, 0, create_color(128, 128, 128))
+    set_color(ttti, 2, 0, create_color(255, 255, 255))
+
+
+testThreeTone()

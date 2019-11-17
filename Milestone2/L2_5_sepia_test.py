5d16d8e607cd7bc9c2b78b04eaac0a31e9142b2f
Anthony Luo <anthonyluo100@gmail.com>
2019-11-17 14:23:30 -0500

    Sepia Test

--- Milestone2/L2_5_sepia_test.py
+++ Milestone2/L2_5_sepia_test.py
@@ -1,12 +1,12 @@
-from Utils.Cimpl import choose_file, load_image
+from Utils.Cimpl import *
 from L2_5_P4_sepia import sepia
 
-def sepia_test() -> None:
+def sepiaTest() -> None:
     """Test function for the sepia filter that checks if all the r, g, b components have gotten the sepia filter applied to them. The function returns "PASS" if all the pixels are successfully changed. If "PASS" is returned, this indicates that the red component has increased by the correct percentage and the blue component has decreased by the correct percentage, ultimately applying the sepia filter. If not, the function returns "FAIL". 
     
     Written by Alia Nichol.
     """
-    img = sepia(load_image(choose_file())
+    img = sepia(load_image(choose_file()), disp = False)
     for x, y, (r, g, b) in img:
         if 0<= r <= 63:
             if g == r / 1.1 and g == b / 0.9:
@@ -23,7 +23,9 @@ def sepia_test() -> None:
         else:
             print("FAIL")
             break
-           
+
+if __name__ == '__main__':
+    sepiaTest()
            
         
         

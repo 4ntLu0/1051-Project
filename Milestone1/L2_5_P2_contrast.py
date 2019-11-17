8397dc9760bab289b5861748470f249b2b597cea
Alia79 <57446174+Alia79@users.noreply.github.com>
2019-11-17 16:45:07 -0500

    no underscoresnow

--- Milestone1/L2_5_P2_contrast.py
+++ Milestone1/L2_5_P2_contrast.py
@@ -9,7 +9,7 @@ image1 = copy(image) #creates a copy of the image so it is not overrided when th
 
 def extreme(image: Image) -> Image:
     
-    """ Using a given image, the r, g, b components are altered so that 
+    """ Using a given image, the this function alters the r, g and b components to create a new contrasted image. 
     Written by Alia Nichol. 
     """   
     
@@ -37,33 +37,3 @@ def extreme(image: Image) -> Image:
     return image1
     
 
-""" DOES NOT WORK BUT NOT SURE WHY
-
-for x, y, (r, g, b) in image:  # reads through each pixel in the image
-       
-       
-        if 0 < r < 127:
-            contrast = create_color(0, g, b)
-            set_color(image, x, y, contrast)            
-        elif 0 < g < 127:
-            contrast = create_color(r, 0, b)
-            set_color(image, x, y, contrast)              
-        elif 0 < b < 127:
-            contrast = create_color(r, g, 0)
-            set_color(image, x, y, contrast)              
-        elif 128 < r < 255:
-            contrast = create_color(255, g, b)
-            set_color(image, x, y, contrast)              
-        elif 128 < g < 255:
-            contrast = create_color(r, 255, b)
-            set_color(image, x, y, contrast)             
-        elif 128 < b < 255:
-            contrast = create_color(r, g, 255)
-            set_color(image, x, y, contrast)                 
-
-    save_as(image, 'contrast_channel.png') # saves the image 
-    show(load_image('contrast_channel.png'))
-    print('contrast_channel saved as new image')
-    
-    return image1"""
-    

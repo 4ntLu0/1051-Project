7ed993c51a04106e97004c1469f559d0bdf0d51a
EmilioL53 <57446542+EmilioL53@users.noreply.github.com>
2019-11-17 14:03:15 -0500

    posterize finished

--- Milestone2/L2_5_P4_posterize.py
+++ Milestone2/L2_5_P4_posterize.py
@@ -1,6 +1,8 @@
-from Cimpl import *
-
-image = load_image(choose_file())
+from Utils.Cimpl import *
+"""
+written by Emilio Lindia
+"""
+image = load_image(choose_file()) #allows you to choose a file
 show(image)
 new_image = copy(image) #creates the copy to prevent it from being overwritten
 
@@ -19,18 +21,18 @@ def _adjust_component(comp: int)-> int:  #enters a red, green, or blue component
     elif comp < 0:
         return 31
 #the midpoint of it's quadrant is returned
+# this function is a helper function that will be called upon
 
 
 
 
-
-def posterize(image: Image) -> Image:
+def posterize(image: Image) -> Image: 
     
-    new_image = copy(image)
-    for pixel in image:
+    new_image = copy(image) #assigns new image as a copy of original
+    for pixel in image:     #examines all pixels in the new image
         x,y,(r,g,b) = pixel
-        (r,g,b) = (_adjust_component(r),_adjust_component(g),_adjust_component(b))
-        new_color = create_color(r,g,b)
+        (r,g,b) = (_adjust_component(r),_adjust_component(g),_adjust_component(b)) 
+        new_color = create_color(r,g,b) #inserts rgb values into adjust component function to change rgb values
         set_color (new_image, x, y, new_color)
     show(new_image)
     return image  

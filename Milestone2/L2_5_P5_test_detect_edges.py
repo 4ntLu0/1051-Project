e00959f15f4191a64c90a2244dc36d361dbccf1a
Anthony Luo <anthonyluo100@gmail.com>
2019-11-19 09:33:56 -0500

    Edge Detection & Testing

--- Milestone2/L2_5_P5_test_detect_edges.py
+++ Milestone2/L2_5_P5_test_detect_edges.py
@@ -1,4 +1,6 @@
-from Cimpl import load_image, choose_file, get_color, Image, get_width, get_height, set_color, create_color, create_image, show, save_as
+from Cimpl import load_image, choose_file, get_color, Image, get_width, get_height, set_color, create_color, \
+    create_image, show, save_as
+from L2_5_P5_detect_edges import edgeDetect
 
 '''
 should be creating image with predet contrast?
@@ -23,7 +25,7 @@ make sure that there is no diagonal influence
 This should create final test image below:
         1      2      3      4      5                    1       2      3      4       5
     1    low    low    low    low    low     --->    1   Blank   Blank  Edge   Blank   Blank
-    2    low    low    high   low    low     --->    2   Blank   Blank  Blank  Blank   Blank
+    2    low    low    high   low    low     --->    2   Blank   Blank  Edge   Blank   Blank
     3    low    low    low    low    low     --->    3   Blank   Blank  Blank  Blank   Blank
     
     --- Side - Side testing:
@@ -36,3 +38,25 @@ This should create final test image below:
 Memefied by Anthony Luo    
 '''
 
+
+def testEdgeDetector():
+    test_img = create_image(5, 3, create_color(30, 10, 20))  # you don't want to create something that is 100% black
+    # or
+    # white, as then it's difficult to tell what errors are created.
+    set_color(test_img, 2, 1, create_color(250, 240, 230))
+
+    compare_img = create_image(5, 2, create_color(255, 255, 255))
+    set_color(compare_img, 2, 0, create_color(0, 0, 0))
+    set_color(compare_img, 2, 1, create_color(0,0,0))
+
+    test_img = edgeDetect(test_img, 100, False, False)
+    for x, y, (r, g, b) in test_img:
+        if (r, g, b) == tuple(get_color(compare_img, x, y)):
+            pass
+        else: print('fails at', x, y); exit()
+
+    print('PASS')
+
+
+if __name__ == '__main__':
+    testEdgeDetector()

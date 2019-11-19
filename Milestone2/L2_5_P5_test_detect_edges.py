1b4df640c7ec7a71834fc620186cbce0697ef924
Anthony Luo <anthonyluo100@gmail.com>
2019-11-19 09:48:26 -0500

    Testing code for edge detection (basic)
    compares pixels, does smol boi testing, very epic.

--- Milestone2/L2_5_P5_test_detect_edges.py
+++ Milestone2/L2_5_P5_test_detect_edges.py
@@ -1,61 +1,77 @@
 from Cimpl import load_image, choose_file, get_color, Image, get_width, get_height, set_color, create_color, \
     create_image, show, save_as
+from typing import Tuple
 from L2_5_P5_detect_edges import edgeDetect
 
-'''
-should be creating image with predet contrast?
-create rows of pixels, BRIGHT dark BRIGHT with a little variance in between (ie, two halves?)
-
-Test cases --- (high / low refer to brightness levels).
-directly below it is right colour
-    high
-    low
-make sure ones to left/right are not influenced
-    low    high                 should not return edges at ALL
-    low    high
-make sure that it is not influenced by the one above it
-    high                        should only return:   edge
-    low                                               no edge
-    low                                               no edge
-make sure that there is no diagonal influence
-    low    low    low    low    low
-    low    low    high   low    low
-    low    low    low    low    low
-    
-This should create final test image below:
-        1      2      3      4      5                    1       2      3      4       5
-    1    low    low    low    low    low     --->    1   Blank   Blank  Edge   Blank   Blank
-    2    low    low    high   low    low     --->    2   Blank   Blank  Edge   Blank   Blank
-    3    low    low    low    low    low     --->    3   Blank   Blank  Blank  Blank   Blank
-    
-    --- Side - Side testing:
-        2,2 - 3,2 & 3,2 - 4,2.
-    --- Up - Down testing:
-        3,1 - 3, 2 & 3,2 - 3,3.
-    --- Diagonal Testing
-        2,1 & 2,3 & 3,2 & 4,1 & 4,3
-
-Memefied by Anthony Luo    
-'''
-
-
 def testEdgeDetector():
-    test_img = create_image(5, 3, create_color(30, 10, 20))  # you don't want to create something that is 100% black
-    # or
-    # white, as then it's difficult to tell what errors are created.
-    set_color(test_img, 2, 1, create_color(250, 240, 230))
+    ''' Tests to make sure that edge detection is functioning as it should with certain threshold values.
+    Test cases --- (high / low refer to brightness levels).
+    directly below it is right colour
+        high
+        low
+    make sure ones to left/right are not influenced
+        low    high                 should not return edges at ALL
+        low    high
+    make sure that it is not influenced by the one above it
+        high                        should only return:   edge
+        low                                               no edge
+        low                                               no edge
+    make sure that there is no diagonal influence
+        low    low    low    low    low
+        low    low    high   low    low
+        low    low    low    low    low
 
-    compare_img = create_image(5, 2, create_color(255, 255, 255))
-    set_color(compare_img, 2, 0, create_color(0, 0, 0))
-    set_color(compare_img, 2, 1, create_color(0,0,0))
+    This should create final test image below:
+            1      2      3      4      5                    1       2      3      4       5
+        1    low    low    low    low    low     --->    1   Blank   Blank  Edge   Blank   Blank
+        2    low    low    high   low    low     --->    2   Blank   Blank  Edge   Blank   Blank
+        3    low    low    low    low    low     --->    3   Blank   Blank  Blank  Blank   Blank
 
-    test_img = edgeDetect(test_img, 100, False, False)
+        --- Side - Side testing:
+            2,2 - 3,2 & 3,2 - 4,2.
+        --- Up - Down testing:
+            3,1 - 3, 2 & 3,2 - 3,3.
+        --- Diagonal Testing
+            2,1 & 2,3 & 3,2 & 4,1 & 4,3
+
+    The same test will be run with a threshold higher than expected, which should return a completely blank image.
+    '''
+    test_pass = True # so far, all tests have passed.
+    test_img, compare_img = _createImages(0)
+    test_img = edgeDetect(test_img, 100, False, False) # this thresh of 100 is a decent average to be testing on.
     for x, y, (r, g, b) in test_img:
         if (r, g, b) == tuple(get_color(compare_img, x, y)):
             pass
-        else: print('fails at', x, y); exit()
+        else: print('error at', x, y); test_pass = False
 
-    print('PASS')
+    test_img, compare_img = _createImages(1)
+    test_img = edgeDetect(test_img, 255, False, False) # this thresh of 255 is higher than we should ever be getting
+    for x, y, (r, g, b) in test_img:
+        if (r, g, b) == tuple(get_color(compare_img, x, y)):
+            pass
+        else: print('error at', x, y); test_pass = False
+
+    if test_pass:
+        print('### Testing done ### \n--- PASSES')
+    else:
+        print('### Testing done ### \n--- FAILS')
+
+def _createImages(ver: int) -> Tuple[Image]:
+    """ Creates the test images for each 'version' of the test.
+    Written by Anthony Luo
+    :param ver: version0: modified compare
+                version1: non-modifide compare
+    :type ver: int
+    """
+    test_img = create_image(5, 3, create_color(30, 10, 20))
+    set_color(test_img, 2, 1, create_color(250, 240, 230))
+    compare_img = create_image(5, 2, create_color(255, 255, 255))
+    if ver == 0:
+        set_color(compare_img, 2, 0, create_color(0, 0, 0))
+        set_color(compare_img, 2, 1, create_color(0, 0, 0))
+        return test_img, compare_img
+    if ver == 1:
+        return test_img, compare_img
 
 
 if __name__ == '__main__':

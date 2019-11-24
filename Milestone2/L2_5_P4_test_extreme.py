c1dc1533e100ce21b7c6a1848bc9110bd595f075
EmilioL53 <57446542+EmilioL53@users.noreply.github.com>
2019-11-24 18:45:42 -0500

    edits done to code

--- Milestone2/L2_5_P4_test_extreme.py
+++ Milestone2/L2_5_P4_test_extreme.py
@@ -1,17 +1,30 @@
 from L2_5_P4_extreme import extreme_contrast
-from Cimpl import create_color, set_color, save_as, show, load_image, Image, choose_file, copy
+from Cimpl import create_color, set_color, save_as, show, load_image, Image,\
+     choose_file, copy
 """
 CODE ATTRIBUTED FROM FILES ON CULEARN
-test function that reviews the code for the contrasting filter to see if the pixels are properly
+test function that reviews the code for the contrasting filter to see if the /
+pixels are properly
 analyzed, if the original image changes and if the image has been contrasted
 
-written by Emilio
+
+HOW TO RUN CODE
+Run the file
+pixels of chosen image are analyzed and a contrasted version of the
+image will appear
+END OF CODE
+
+
+
+
+written by Emilio: 101143244
 """
 
 
 def test_extreme():
-    image = load_image(choose_file())  # loads a file that you choose
-    image1 = extreme_contrast(image)  # image 1 is the updated version
+    
+    image = load_image( choose_file ( ) )  # Loads a file that you choose
+    
     
     for x, y, (r, g, b) in image:
         print('this pixel has been analyzed correctly')  
@@ -20,7 +33,7 @@ def test_extreme():
         print('this pixel has not been analyzed correctly')  
         
             
-    if image1 == extreme_contrast(image1):
+    if image == extreme_contrast(image):
         print('the image contrast has: PASSED THE TEST')
     else:
         print('the image contrast has: FAILED THE TEST')

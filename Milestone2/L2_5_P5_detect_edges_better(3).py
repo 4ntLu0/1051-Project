b0583fe1434014757ad9ab56d59543050ff451d0
Alia79 <57446174+Alia79@users.noreply.github.com>
2019-11-24 09:51:47 -0500

    hihi

---
+++ Milestone2/L2_5_P5_detect_edges_better(3).py
@@ -0,0 +1,90 @@
+from Cimpl import load_image, choose_file, get_color, Image, get_width, get_height, set_color, create_color, \
+    create_image, show, save_as
+import os
+
+'''
+Written by Anthony Luo, Nov 20, 2019
+Cimpl attributed from CuLearn, 2019.
+'''
+
+
+def detect_edges_better(img: Image, thresh: int = 0, disp: bool = True, save: bool = True):
+    """ return an image with the edge detects.
+    Written by Anthony Luo (#101145222)
+    :param img: Image to detect edges on
+    :param thresh: Threshold value for detection
+    :param disp: Whether or not to display the image, True by default
+    :param save: Whether or not ot save the image. True by default.
+    :return: edge detected image.
+    >>> detect_edges_better(load_image(choose_file()), 13)
+    >>> detect_edges_better(load_image(choose_file()), 15, False, True)
+    """
+
+    # functional definitions
+    width = get_width(img)
+    height = get_height(img)
+    new_img = create_image(width, height)
+    white = create_color(255, 255, 255)
+    black = create_color(0, 0, 0)
+
+    for y in range(height - 1):  # starts at the first level, then moves down
+        for x in range(width - 1):
+            # compares brightness between two images and then resets colours.
+            if abs(_avg_bright(tuple(get_color(img, x, y))) - _avg_bright(
+                    tuple(get_color(img, x, y + 1)))) > thresh or abs(
+                    _avg_bright(tuple(get_color(img, x, y))) - _avg_bright(tuple(get_color(img, x + 1, y)))) > thresh:
+                # sets colour to black (edge detected)
+                set_color(new_img, x, y, black)
+            else:
+                # sets colour to white (no edge)
+                set_color(new_img, x, y, white)
+
+    for x in range(width):
+        # sets the last row of pixels to be equal to the one right above
+        set_color(new_img, x, height - 1,
+                  _new_col(get_color(new_img, x, height - 2)))
+
+    for y in range(height):
+        # sets last column of pixels to be equal to the ones to the left
+        set_color(new_img, width - 1, y,
+                  _new_col(get_color(new_img, width - 2, y)))
+
+    # sets the colour for the last pixel
+    if get_color(new_img, width - 2, height - 1) == create_color(0, 0, 0) or \
+            get_color(new_img, width - 1, height - 2) == create_color(0, 0, 0):
+        set_color(new_img, width - 1, height - 1, create_color(0, 0, 0))
+    else:
+        set_color(new_img, width - 1, height -
+                  1, create_color(255, 255, 255))
+    if disp:
+        show(new_img)  # shows image
+    if save:
+        # saves to returns/
+        save_as(new_img, 'returns/better_edge_detect.png')
+    return new_img
+
+
+def _avg_bright(colour):
+    """Returns avg brightness of three colours
+    written by anthony luo
+    """
+    r, g, b = colour
+    return (r + g + b) / 3
+
+
+def _new_col(colour):
+    """ Returns a colour type from a colour
+    written by Anthony Luo
+    """
+    # TODO: try to see if there's a way to integrate this directly into the program
+    r, g, b = tuple(colour)
+    return create_color(r, g, b)
+
+
+if __name__ == '__main__':
+    new_path = os.getcwd() + '/returns'  # creates path
+    try:
+        os.mkdir(new_path)
+    except:
+        pass
+    ret_img = detect_edges_better(load_image(choose_file()), 15)
--- Milestone2/L2_5_P5_detect_edges_better_test.py
+++ Milestone2/L2_5_P5_detect_edges_better_test.py
@@ -2,60 +2,51 @@
 
 from Cimpl import load_image, choose_file, get_color, Image, get_width, get_height, set_color, create_color,\
    create_image, show, save_as, copy
-from L2_5_P5_detect_edges_better import detect_edges_better
+from L2_5_P5_detect_edges_better import detect_edges_better      #Imports the Improved Edge Detection function so it can later be called. 
 
-def detect_edges_better_test(image:Image) -> str:
-    """Test function that checks if the r, g, b components have been correctly altered by the Improved Edge Detection filter. If the pixels are correctly changed to either white or black, the test function returns "PASS" to the user. If this is not the case, test function returns "FAIL".
+def detect_edges_better_test() -> None:
+    """Test function that checks if the r, g, b components have been correctly altered by the Improved Edge Detection filter. \
+    If the pixels are correctly changed to either white or black, the test function returns "PASS" to the user. If this is not the case, \
+    the test function returns "FAIL".
     
     Written by Alia Nichol (#101143486). 
-    >>> edgeTest(image)
+    >>> detect_edges_better_test(image)
     PASS
     
-    >>> edgeTest(image)
+    >>> detect_edges_better_test(image)
     FAIL
     """
     
-    image = create_image(3, 4)  # Creates image that is 
-    set_color(image, 0, 0, create_color(10, 20, 50))
-    set_color(image, 1, 0, create_color(80, 100, 180))
-    set_color(image, 2, 0, create_color(200, 240, 243))  
-    set_color(image, 2, 1, create_color(200, 240, 243))  
-    set_color(image, 2, 2, create_clor(200, 240, 243))   
-      
+    original_image = create_image(7, 3, color = create_color(10, 10, 10))       # Creates image that is width 6 and height 2 set to a dark colour. 
+    bright = create_color(250, 250, 250)
+    set_color(original_image, 1, 0, bright)
+    set_color(original_image, 2, 1, bright)
+    set_color(original_image, 2, 2, bright)
+    set_color(original_image, 3, 0, bright)
+    set_color(original_image, 3, 1, bright)
+    set_color(original_image, 3, 2, bright)
+    set_color(original_image, 4, 1, bright)         
     
-    width = (get_width(image) - 1)
-    height = (get_height(image) - 1)  
-
-    pixel_count = 0 #Creating a counter to count the number of pixels in the image. 
-    correct = 0  #Creating a counter to count the number of pixels that have been correctly changed. 
+    expected_image =  create_image(7, 3, color = create_color(255, 255, 255))   # Creates image that is width 6 and height 2 set to a bright colour.
+    black = create_color(0, 0, 0)
+    set_color(expected_image, 0, 0, black)
+    set_color(expected_image, 1, 0, black)
+    set_color(expected_image, 1, 1, black)  
+    set_color(expected_image, 1, 2, black)    
+    set_color(expected_image, 2, 0, black)
+    set_color(expected_image, 3, 0, black)
+    set_color(expected_image, 4, 0, black)  
+    set_color(expected_image, 4, 1, black)    
+    set_color(expected_image, 4, 2, black)    
     
-    new_img = detect_edges_better(image)
-
-    for y in range(height):
-        for x in range(width):
-            pixel_count += 1 
-
-            r, g, b = tuple(get_color(image, x, y))
-            r1, g1, b1 = tuple(get_color(image, x, y + 1))  # Accounting for the pixels below
-            r2, g2, b2 = tuple(get_color(image, x + 1, y))  # Accounting for the pixels to the right
-            r3, g3, b3 = tuple(get_color(new_image, x, y))
-
-            if int(abs(((r + g + b) / 3) - ((r1 + g1 + b1) / 3))) > threshold and int(abs(((r + g + b) / 3) - ((r2 + g2 + b2) / 3))) > threshold and r3 == 0 and g3 ==0 and b3 ==0:
-                correct += 1
- 
-            elif int(abs(((r + g + b) / 3) - ((r1 + g1 + b1) / 3))) <= threshold \
-                 and int(abs(((r + g + b) / 3) - ((r2 + g2 + b2) / 3))) <= threshold and r3 == 255 and g3 == 255 and b3 == 255:
-                correct += 1
-
-    if pixel_count == correct:
-        return print("PASS")
-    
-    else:
-        return print("FAIL")
-    
-    #show(new_img)
-    
-    
-
-
-    
+    actual_image = detect_edges_better(original_image, 100, False)              # The Improved Edge Detection filter is applied to original_image. 
+       
+    for x, y, (r, g, b) in actual_image:
+        if (r, g, b) == tuple(get_color(expected_image, x, y)):                 # Checks if the pixels in the actual image equal the ones in the expected image.
+            print('PASS')
+        else:
+            print('FAIL')
+        
+detect_edges_better_test()                                                      # Calls the test function.
+                
+                

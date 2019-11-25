0e2eaad3736e399c8db392572eae5c07b72bf872
Anthony Luo <anthonyluo100@gmail.com>
2019-11-25 09:07:53 -0500

    Update L2_5_interactive_ui.py

--- L2_5_interactive_ui.py
+++ L2_5_interactive_ui.py
@@ -3,6 +3,7 @@ from L2_5_image_filters import create_red, create_blue, green_filter, combine, e
 from typing import Tuple
 from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height, copy, \
     create_image, save_as, get_color, choose_file, copy
+import os
 
 
 def prompt():
@@ -11,10 +12,16 @@ def prompt():
     """
 
 
-def apply_filter(selection: str, image: Image) -> Image:
+def apply_filter(selection, follow_up, image: Image) -> Image:
     """
     Applies the selected filter
     """
+    functions = [two_tone, three_tone, extreme_contrast, sepia, posterize, detect_edges, detect_edges_better, flip_vertical, flip_horizontal]
+    prompts = ['2', '3', 'X', 'T', 'P', 'E', 'I', 'V', 'H']
+    for index in range(prompts):
+        if selection == prompt[index]:
+            fn = functions[index]
+            return fn(image)
 
 
 def main():

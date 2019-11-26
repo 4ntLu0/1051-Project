cddc296689520a31db0620ae68a07bda741424f6
Anthony Luo <anthonyluo100@gmail.com>
2019-11-26 11:46:41 -0500

    a few smol updates!

--- L2_5_interactive_ui.py
+++ L2_5_interactive_ui.py
@@ -5,18 +5,22 @@ from Cimpl import load_image, create_color, set_color, show, Image, save_as, get
     create_image, save_as, get_color, choose_file, copy
 import os
 
-all_commands = ["L", "l", "S", "s", "2", "3", "X", "x", "T", "t", "P", "p", "E", "e", "I", "i", "V", "v", "H", "h", "Q", "q"]
+all_commands = ["L", "l", "S", "s", "2", "3", "X", "x", "T", "t", "P", "p", "E", "e", "I", "i", "V", "v", "H", "h", "Q",
+                "q"]
 
-def prompt() ->str :
+
+def prompt() -> str:
     """
     Prompts the user for an input. Checks validility of the input before the filter is applied.     
     """
     command = None
     while command not in all_commands:
         print("Prompt is not valid. Please Try Again.")
-        command = input("Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n Command:")
-        
-        return(command)
+        command = input(
+            "Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n Command:")
+        command = command.upper()
+        return (command)
+
 
 def is_valid():
     """
@@ -25,23 +29,35 @@ def is_valid():
     :rtype:
     """
 
+
 def apply_filter(selection: str, image: Image) -> Image:
     """
     Applies the selected filter.
     """
 
-    functions = [two_tone, three_tone, extreme_contrast, sepia, posterize, detect_edges, detect_edges_better, flip_vertical, flip_horizontal]
+    functions = [two_tone, three_tone, extreme_contrast, sepia, posterize, detect_edges, detect_edges_better,
+                 flip_vertical, flip_horizontal]
     prompts = ['2', '3', 'X', 'T', 'P', 'E', 'I', 'V', 'H']
-    for index in range(len(prompts)):
-        if selection == prompts[index]:
-            fn = functions[index]
-            return fn(image)
-    
+    if selection == '2':
+        # two tone filter selected
+    elif selection == '3':
+        # three tone filter selected
+    elif selection == 'E'
+        # edge detection selected
+    elif selection == 'I':
+        # improved edge detection selected
+    else:
+        for index in range(len(prompts)):
+            if selection == prompts[index]:
+                fn = functions[index]
+                return fn(image)
+
 
 def main():
     image = load_image(choose_file())
     while True:
         image = apply_filter(prompt(), image)
+
 
 if __name__ == '__main__':
     new_path = os.getcwd() + '/returns'  # creates path

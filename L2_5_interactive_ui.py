ff9c85fd7d7385254f86880a322f2402ca539d6a
Alia79 <57446174+Alia79@users.noreply.github.com>
2019-11-27 09:10:11 -0500

    hii

--- L2_5_interactive_ui.py
+++ L2_5_interactive_ui.py
@@ -5,36 +5,34 @@ from L2_5_image_filters import create_red, create_blue, green_filter, combine, e
     two_tone, three_tone, detect_edges, detect_edges_better, flip_horizontal, flip_vertical
 from typing import Tuple
 from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height, copy, \
-    create_image, save_as, get_color, choose_file, copy
+    create_image, get_color, choose_file, copy
 import os
 
 all_commands = ["L", "S", "2", "3", "X", "T", "P", "E", "I", "V", "H", "Q"]
-
 
 def prompt() -> str:
     """
     Prompts the user for an input. Checks validility of the input before the filter is applied.     
     """
-    command = input("Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n : ")
-    command = command.upper()
+    command = input("Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n : ").upper()
+    #command = command.upper()
+    
     while command not in all_commands:
-        print("Prompt is not valid. Please Try Again.")
-        command = input("Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n : ")
-        command = command.upper()
-        return (command)
-
+        print("No such command")
+        command = input("Please enter a command from the following list: \n L)oad Image S)ave-as \n 2)-tone 3)-tone X)treme contrast T)int sepia P)osterize \n E)dge detect I)mproved edge detect V)ertical flip H)orizontal flip \n Q)uit \n : ").upper()
+        #command = command.upper()
+        return command
 
 def image_load():
+    """
+    """
     image = input("Name of image to load:")
-    loaded_image = load_image(image)  # use choose?
+    loaded_image = load_image(image)  
     show(loaded_image)
     print("Image has been loaded")
-    load = True
-    return loaded_image, load
+    return loaded_image
 
-
-
-def apply_filter(selection: str, image: Image) -> Image:
+def apply_filter(selection: str, loaded_image: Image) -> Image:
     """
     Applies the selected filter.
     """
@@ -42,34 +40,39 @@ def apply_filter(selection: str, image: Image) -> Image:
     functions = [two_tone, three_tone, extreme_contrast, sepia, posterize, detect_edges, detect_edges_better,
                  flip_vertical, flip_horizontal]
     prompts = ['2', '3', 'X', 'T', 'P', 'E', 'I', 'V', 'H']
+    
     if selection == '2':
         color1 = input("First colour:")
         color2 = input("Second colour:")
         new_image = two_tone(loaded_image, color1, color2)
         show(new_image)
         print("Two tone filter has been applied")
+        
     elif selection == '3':
         color1 = input("First colour:")
         color2 = input("Second colour:")
         color3 = input("Third colour:")
         new_image = three_tone(loaded_image, color1, color2, color3)
         show(new_image)
-        print("Three tone filter has been applied")        
+        print("Three tone filter has been applied") 
+        
     elif selection == 'E':
         thresh = input('Threshold: ')
         new_image = detect_edges(loaded_image, thresh)
         show(new_image)
         print("Edge Detection filter has been applied")
+        
     elif selection == 'I':
         thresh = input('Threshold: ')
         new_image = detect_edges_better(loaded_image, thresh)
         show(new_image)
         print("Improved Edge Detection filter has been applied")
+        
     else:
         for index in range(len(prompts)):
             if selection == prompts[index]:
                 fn = functions[index]
-                return fn(image)
+                return fn(loaded_image)
 
 
 def main():

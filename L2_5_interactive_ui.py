76741a9074f00d5ceb4341eee440e5aebce8291b
Anthony Luo <anthonyluo100@gmail.com>
2019-11-28 10:42:25 -0500

    Pls refine readme

--- L2_5_interactive_ui.py
+++ L2_5_interactive_ui.py
@@ -3,15 +3,13 @@ This code has been attributed from given files on cuLearn."""
 
 # TODO:
 #  - P8 need to finish the README (Contact info, the date, software"s name, small description, installation requirements, etc)
-#  - make sure that the image is shown at the correct times and # of times
+#  - make sure that the image is shown at the correct times and # of times SHOULD BE DONE NOW
 #  - complete the docstrings and the type annotations
 #  - comment the code
 
-from L2_5_image_filters import create_red, create_blue, green_filter, combine, extreme_contrast, posterize, sepia, \
-    two_tone, three_tone, detect_edges, detect_edges_better, flip_horizontal, flip_vertical, Image
-from typing import Tuple
-from Cimpl import load_image, create_color, set_color, show, Image, save_as, get_width, get_height, copy, \
-    create_image, get_color, choose_file, copy
+from L2_5_image_filters import extreme_contrast, posterize, sepia, two_tone, three_tone, detect_edges, \
+    detect_edges_better, flip_horizontal, flip_vertical, Image
+from Cimpl import load_image,show, Image, save_as, copy
 import os
 
 all_commands = ["L", "S", "2", "3", "X", "T", "P", "E", "I", "V", "H", "Q"]
